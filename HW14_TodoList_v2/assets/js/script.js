188d763116aa31e5e72412fa2008a5a7c1c4055a
Yulia Demchenko <julia95demchenko@gmail.com>
2020-02-03 23:15:17 +0200

    HW14 - Fixed url path and code refactored.

--- HW14_TodoList_v2/assets/js/script.js
+++ HW14_TodoList_v2/assets/js/script.js
@@ -1,23 +1,26 @@
 "use strict";
 
-fetch('https://jsonplaceholder.typicode.com/photos?_limit=50')
-    .then(response => {
-        response.json()
-        .then(data => {
-            data.forEach(item => {
-                addItemToList(item.title);
-            })
-        });
-    });
-
 const ulistElement = document.querySelector("#toDoList");
 const liTemplate = document.querySelector("#liTemplate").innerHTML;
-let currentItemNum = 1;
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => {
+        response.json()
+        .then(renderTodos);
+    });
 
 document.querySelector("#toDoList").addEventListener("click", markListItem);
 
-function addItemToList(item) {
-    ulistElement.innerHTML += liTemplate.replace("{{todo}}", item).replace("{{currentNumber}}", currentItemNum++);
+
+function renderTodos(data) {
+    data.forEach(item => {
+        addItemToList(item.id, item.title, item.completed);
+    });
+}
+function addItemToList(id, item, isCompleted) {
+    ulistElement.innerHTML += liTemplate.replace("{{id}}", id)
+                                        .replace("{{todo}}", item)
+                                        .replace("{{completed}}", isCompleted ? "done" : "");
 }
 function markListItem(e) {
     const currentElement = e.target;
