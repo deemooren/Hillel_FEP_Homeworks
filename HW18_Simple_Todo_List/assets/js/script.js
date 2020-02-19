62bd1a1f29c1e287d04402f514ef2c9ced88d6ce
deemooren <bugakovayk@gmail.com>
2020-02-19 15:24:29 +0200

    Homework 18 - Simple To-do List

---
+++ HW18_Simple_Todo_List/assets/js/script.js
@@ -0,0 +1,100 @@
+const TODOS_KEY = 'todos';
+
+const $todoListElement = $('.todo-list');
+const $addTodoForm = $('#addTodoForm');
+let todos = [];
+
+init();
+
+$addTodoForm.submit(onSubmitForm);
+$todoListElement.click('.todo-item', onTodoItemClick);
+
+function init() {
+    getTodos();
+}
+function onSubmitForm(e) {
+    e.preventDefault();
+    addTodo();
+    resetForm();
+}
+function onTodoItemClick(e) {
+    const $target = $(e.target);
+
+    if($target.hasClass('delete-btn')) {
+        deleteDotoItem($target.parent().data('id'));
+    } else {
+        editTodoItem($target.data('id'));
+    }
+}
+function addTodo() {
+    const text = $('#todoInput').val().trim();
+
+    if(text.length !== 0) {
+        const newTodo = createNewTodo(text);
+        todos.push(newTodo);
+        saveChanchingInLocalStorage(todos);
+
+        const pushedTodo = getElementFormArrayById(newTodo.id);
+        renderTodoItem(pushedTodo);
+    }
+}
+function getTodos() {
+    let data = localStorage.getItem(TODOS_KEY);
+    data = data ? JSON.parse(data) : [];
+
+    setTodos(data);
+    renderTodos(data);
+}
+function setTodos(data) {
+    return (todos = data);
+}
+function renderTodos(data) {
+    data.forEach(item => {
+        renderTodoItem(item);
+    });
+}
+function renderTodoItem(todo) {
+    const $listTemplate = $('#liElementTemplate').html();
+    const $todo = $($listTemplate.replace('{{to-do}}', todo.title)
+                                 .replace('{{id}}', todo.id));
+    if(todo.isDone) {
+        $todo.addClass('done');
+    }
+
+    $todoListElement.append($todo);
+}
+function createNewTodo(value) {
+    return {
+        id: Date.now(),
+        title: value,
+        isDone: false
+    };
+}
+function deleteDotoItem(id) {
+    const $currElement = getDOMElementByDataId(id);
+    $currElement.remove();
+
+    todos = todos.filter(todo => todo.id !== Number(id));
+    saveChanchingInLocalStorage(todos);
+}
+function editTodoItem(id) {
+    const $currElement = getDOMElementByDataId(id);
+    $currElement.toggleClass('done');
+
+    const currItem = getElementFormArrayById(id);
+    const currStatus = currItem.isDone;
+    currItem.isDone = currStatus ? false : true;
+    saveChanchingInLocalStorage(todos);
+}
+function saveChanchingInLocalStorage(todos) {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+function resetForm() {
+    $addTodoForm.trigger("reset");
+}
+function getDOMElementByDataId(id) {
+    return $(`.todo-item[data-id='${id}']`);
+}
+function getElementFormArrayById(id) {
+    return todos.find(item => item.id === id);
+}
