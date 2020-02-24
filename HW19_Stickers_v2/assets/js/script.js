2bc29a9900f4ecb4ce13984a0a35fc1f3783c334
jbugakova <bugakovayk@gmail.com>
2020-02-24 10:26:51 +0200

    Implemented sticker resizing.

--- HW19_Stickers_v2/assets/js/script.js
+++ HW19_Stickers_v2/assets/js/script.js
@@ -1,14 +1,17 @@
 const STICKER_CLASS = 'sticker';
 const HIDDEN_CLASS = 'hidden';
 const KEY_STICKERS = 'stickers';
+const MAX_STICKERS_QUANTITY = 42;
+const STICKERS_STEP = 30;
 
 const $stickersContainer = $('#stickersContainer');
 const $errorMessage = $('#errorMessage');
 const $panel = $('#panel');
 let currentZIndex = 1;
 let stickers = [];
+let lastEditedStickerID = 0;
 
-const dialog = $('#dialogForm').dialog({
+const dialogForm = $('#dialogForm').dialog({
     autoOpen: false,
     height: 340,
     width: 350,
@@ -18,10 +21,19 @@ const dialog = $('#dialogForm').dialog({
         CANCEL: closeDialogWindow
     }
 });
+const dialogWarning = $('#dialogWarning').dialog({
+    autoOpen: false,
+    height: 85,
+    width: 350,
+    hide: {
+    effect: "fade",
+    duration: 400
+    }
+});
 
 init();
 
-$panel.on('click', '#addStickerBtn', openDialogWindow);
+$panel.on('click', '#addStickerBtn', onAddStickerClick);
 $panel.on('click', '#resetPositionsBtn', onResetBtnClick);
 $stickersContainer.on('click', '.delete-btn', onDeleteBtnClick);
 $stickersContainer.on('focus', '.sticker', onStickerFocus);
@@ -30,13 +42,18 @@ $stickersContainer.on('blur', '.sticker', onStickerBlur);
 function init() {
     getStickers();
 }
-function openDialogWindow() {
-    dialog.dialog( 'open');
+function onAddStickerClick() {
+    if(stickers.length < MAX_STICKERS_QUANTITY) {
+        openDialogWindow();
+    } else {
+        dialogWarning.dialog('open');
+        setTimeout(() => dialogWarning.dialog('close') , 800);
+    }
 }
 function closeDialogWindow() {
     $errorMessage.addClass(HIDDEN_CLASS);
     $('#addStickerForm').trigger('reset');
-    dialog.dialog('close');
+    dialogForm.dialog('close');
 }
 function onAddBtnClick() {
     const text = $('#addStickerForm #text').val().trim();
@@ -59,16 +76,22 @@ function onStickerFocus(e) {
     refreshZIndex($(e.target).parent());
 }
 function onResetBtnClick() {
-    let topPosition = 0;
+    let topPosition = 20;
     stickers.map(sticker => {
         sticker.positionLeft = 0;
-        sticker.positionTop = (topPosition += 60);
+        sticker.positionTop = topPosition;
+        topPosition += STICKERS_STEP;
+        sticker.customWidth = '250px';
+        sticker.customHeight = '250px';
     });
 
     saveStickersInStorage(stickers);
 
     $stickersContainer.html('');
     renderStickers(stickers);
+}
+function openDialogWindow() {
+    dialogForm.dialog( 'open');
 }
 function getStickers() {
     let data = localStorage.getItem(KEY_STICKERS);
@@ -95,25 +118,59 @@ function renderSticker(item) {
     $newElem.css('left', item.positionLeft);
     $newElem.css('top', item.positionTop);
 
+    $newElem.css('width', item.customWidth);
+    $newElem.css('height', item.customHeight);
+
+    refreshZIndex($newElem);
+
     $newElem.draggable({
         containment: "parent",
         start: (e) => refreshZIndex($(e.target)),
         stop: saveStickerPosition
+    }).resizable({
+        stop: saveStickerSize,
+        maxHeight: 450,
+        maxWidth: 450,
+        minHeight: 250,
+        minWidth: 250
     });
 }
 function createNewSticker(text) {
+    const currnetPosition = getCurrnetPosition();
+
     const sticker = {
         id: Date.now(),
         text: text,
-        positionLeft: '',
-        positionTop: ''
+        positionLeft: currnetPosition.left,
+        positionTop: currnetPosition.top,
+        customWidth: '',
+        customHeight: ''
     };
+    lastEditedStickerID = sticker.id;
     stickers.push(sticker);
     saveStickersInStorage(stickers);
 
     renderSticker(sticker);
 }
+function getCurrnetPosition() {
+    if(lastEditedStickerID === 0) {
+        return {
+            left: 400,
+            top: 0
+        };
+    } else {
+        const lastEditedSticker = getStickerFromArrayByID(lastEditedStickerID);
+        return {
+            left: lastEditedSticker.positionLeft,
+            top: parseInt(lastEditedSticker.positionTop) + STICKERS_STEP
+        };
+    }
+}
 function deleteSticker(id) {
+    if(lastEditedStickerID === id) {
+        lastEditedStickerID = 0;
+    }
+
     stickers = stickers.filter(sticker => sticker.id !== Number(id));
     saveStickersInStorage(stickers);
 
@@ -121,7 +178,7 @@ function deleteSticker(id) {
     currElem.remove();
 }
 function editStickerText(id, newText) {
-    const editingSticker = stickers.find(sticker => sticker.id === Number(id));
+    const editingSticker = getStickerFromArrayByID(id);
     editingSticker.text = newText;
 
     saveStickersInStorage(stickers);
@@ -137,10 +194,25 @@ function refreshZIndex($currElem) {
 }
 function saveStickerPosition() {
     const $currElement = $(this);
-    
-    const currSticker = stickers.find(sticker => sticker.id === Number($currElement.data('id')));
+
+    lastEditedStickerID = $currElement.data('id');
+
+    const currSticker = getStickerFromArrayByID(lastEditedStickerID);
     currSticker.positionLeft = String($currElement.css('left'));
     currSticker.positionTop = String($currElement.css('top'));
 
     saveStickersInStorage(stickers);
+}
+function saveStickerSize() {
+    const $currElement = $(this);
+    const currID = $currElement.data('id');
+    
+    const currSticker = getStickerFromArrayByID(currID);
+    currSticker.customWidth = String($currElement.css('width'));
+    currSticker.customHeight = String($currElement.css('height'));
+
+    saveStickersInStorage(stickers);
+}
+function getStickerFromArrayByID(id) {
+    return stickers.find(sticker => sticker.id === Number(id));
 }
